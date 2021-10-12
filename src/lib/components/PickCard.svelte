ff284bac32ec43ccf98de5260f489e596877f776
Josh Oettinger <j.oettinger@fetchrewards.com>
2021-10-12 12:10:24 -0500

    [MAIN] Added odds

--- src/lib/components/PickCard.svelte
+++ src/lib/components/PickCard.svelte
@@ -18,12 +18,13 @@
     <div class="flex justify-between text-gray-800">
         <div class="flex flex-grow justify-around items-center md:p-3">
             <Team on:click={() => onWinnerPick(matchup.teams[0].abbr)} team={matchup.teams[0]} picked={matchup.pick === matchup.teams[0].abbr}/>
-            <div class="font-semibold text-2xl">
-                vs
+            <div class="flex flex-col text-center items-center">
+                <span class="text-xl md:text-2xl font-semibold">vs</span>
+                <span class="text-sm w-16 md:text-lg md:w-20 font-medium text-gray-600 mt-3">{matchup.odds}</span>
             </div>
             <Team on:click={() => onWinnerPick(matchup.teams[1].abbr)} team={matchup.teams[1]} picked={matchup.pick === matchup.teams[1].abbr}/>
         </div>
-        <div class="flex flex-col items-center justify-center px-4 md:px-10 bg-gray-100" style={dragDisabled ? 'cursor: grab' : 'cursor: grabbing'} on:mousedown={startDrag} on:touchstart={startDrag}>
+        <div class="flex flex-col items-center justify-center px-3 md:px-10 bg-gray-100" style={dragDisabled ? 'cursor: grab' : 'cursor: grabbing'} on:mousedown={startDrag} on:touchstart={startDrag}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
             </svg>
